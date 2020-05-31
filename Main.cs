2cf646acc580d9a3224c9d58328b83becfabcb05
ismaeelakram <mathcenter562@gmail.com>
2020-05-31 17:45:14 -0400

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using InfinityScript;
+using System.Collections.Generic;
+
+namespace Nightingale
+{
+    public class Nightingale : BaseScript
+    {
+        List<Entity> PlayerList;
+
+        public Nightingale()
+        {
+            OnServerStart();
+            PlayerConnected += OnPlayerConnect;
+        }
+
+        public void OnPlayerConnect(Entity player)
+        {
+            PlayerList.Add(player);
+            Players.Add(player);
+
+            player.SetClientDvar("cg_objectiveText", "^3This server is powered by ^1Nightingale ^5by Mahjestic.");
+        }
+
+        public void OnServerStart()
+        {
+
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,50 @@
+﻿using InfinityScript;
+
+namespace Nightingale
+{
+    namespace MajesticScript
+    {
+        public static class WriteLog
+        {
+            public static void None(string message)
+            {
+                Log.Write(LogLevel.None, message);
+            }
+            public static void Info(string message)
+            {
+                Log.Write(LogLevel.Info, message);
+            }
+
+            public static void Error(string message)
+            {
+                Log.Write(LogLevel.Error, message);
+            }
+
+            public static void Warning(string message)
+            {
+                Log.Write(LogLevel.Warning, message);
+            }
+        }
+
+        public static class Chat
+        {
+            public static void SayToAll(string message)
+            {
+                Utilities.RawSayAll(message);
+            }
+            public static void SayToPlayer(Entity player, string message)
+            {
+                Utilities.RawSayTo(player, message);
+            }
+        }
+
+        public class Players
+        {
+            public static Entity FindPlayer(string playerName)
+            {
+                return null;
+            }
+        }
+    }
+
+}
